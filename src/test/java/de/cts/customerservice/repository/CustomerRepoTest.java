1c1989b7f7d00d39924ab77d57c971c3da51b123
ctschubel <ctschubel@googlemail.com>
2020-11-15 13:22:38 +0100

    improved tests for mongodb

--- src/test/java/de/cts/customerservice/repository/CustomerRepoTest.java
+++ src/test/java/de/cts/customerservice/repository/CustomerRepoTest.java
@@ -1,21 +1,44 @@
 package de.cts.customerservice.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
 import de.cts.customerservice.model.Customer;
 
-@SpringBootTest
+/**
+ * @author chris
+ */
+@DataMongoTest
 class CustomerRepoTest {
 
 	@Autowired
 	private CustomerRepo repo;
 
 	@Test
-	void test() {
-		repo.save(new Customer("chris", "ScharnhorstStraße 46", "ctschubel@googlemail.com", 24));
-		repo.findAll().stream().forEach(c -> System.out.println(c));
+	public void save_one_ok() {
+		Customer customer = new Customer("chris", "ScharnhorstStraße 46", "ctschubel@googlemail.com", 24);
+		this.repo.save(customer);
+		List<Customer> customers = this.repo.findAll();
+
+		assertThat(customers.get(0)).isEqualTo(customer);
+	}
+
+	@Test
+	public void save_multiple_ok() {
+		List<Customer> customers = IntStream.range(0, 100)
+				.mapToObj(i -> new Customer("chris", "ScharnhorstStraße 46", "ctschubel@googlemail.com", 24))
+				.collect(Collectors.toList());
+		this.repo.saveAll(customers);
+		List<Customer> actualCustomers = this.repo.findAll();
+
+		assertThat(actualCustomers).isEqualTo(customers);
 	}
 
 }
